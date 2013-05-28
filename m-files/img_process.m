e7724107f0b59eb78e1b83de0352c981444495bf
dmitry <dsultanov@artec-group.com>
2013-05-28 16:12:46 +0400

    all figures masks found

--- m-files/img_process.m
+++ m-files/img_process.m
@@ -6,31 +6,31 @@ function do_process(varargin)
     out_name = 'my_result.png';
     img = imread(in_name);
     img_gray = rgb2gray(img);
-    %figure(1), imshow(img_gray);
     img_dbl = im2double(img_gray);
     img_dbl_comp = imcomplement(img_dbl);
-    %[counts, x] = imhist(img_dbl);
-    %figure(2), stem(x, counts);
+    % find threshold for background color
     Thr = graythresh(img_dbl)*thrCoef;
     bw_mask = im2bw(img_dbl, Thr);
+    % HACK - add right part of ellipse to mask. it is incomplete yet
     bw_mask = im2bw(img_dbl_comp, 0.05) .* bw_mask;
-    % figure(3), imshow(bw_mask);
-    %figure(4), imshow(bw_mask_inv);
+    % background mask
     img_back = bw_mask .* img_dbl;
+    % sum all non-zero points to calculate average background color
     S = 0;
     num = 0;
     W=size(img_back, 2);
     H=size(img_back, 1);
-    x_rect = 0;
     y_ellipse_start = H;
     y_ellipse_stop = 0;
-    xleft = H;
-    xright = 0;
+    xleft_ellipse = H;
+    xright_ellipse = 0;
     segment_n = 0;
-    for y=1:H
-        % find horizontal line
+    % search for ellipse coords
+    % also calculate sum of colors in this loop
+   for y=1:H
+        % find horizontal line, what ellipse starts from
         if y_ellipse_start == H
-            p = y_line_prod( y, img_back);
+            p = y_line_black( y, img_back);
             if segment_n == 0 && p == 0
                 segment_n = 1;
             elseif segment_n == 1 && p ~= 0
@@ -38,7 +38,7 @@ function do_process(varargin)
                 segment_n = 2;
             end
         elseif y_ellipse_stop == 0 && segment_n > 1
-            p = y_line_prod( y, img_back);
+            p = y_line_black( y, img_back);
             if p == 0 && segment_n == 2
                 segment_n = 3;
             end
@@ -46,13 +46,14 @@ function do_process(varargin)
                 y_ellipse_stop = y;
             end
         end
+        % find left and right ellipse boundaries
         for x=1:W
             v = img_back(y, x);
             if y > y_ellipse_start && v == 0
-                if x < xleft
-                    xleft = x;
-                elseif x > xright
-                    xright = x;
+                if x < xleft_ellipse
+                    xleft_ellipse = x;
+                elseif x > xright_ellipse
+                    xright_ellipse = x;
                 end
             end
             if v == 0
@@ -62,31 +63,56 @@ function do_process(varargin)
             num = num + 1;
         end
     end
-    ellipse_mask = ones(size(img_back));
-    xcenter_ellipse = (xleft + xright) / 2;
+    % search for rectanle left coord
+    xleft_rect = 0;
+    segment_n = 0;
+    for x=1:W
+        p = x_line_black(x, y_ellipse_start, bw_mask);
+        if p == 0 && segment_n == 0
+            segment_n = 1;
+        elseif p ~= 0 && segment_n == 1
+            xleft_rect = x;
+            break;
+        end
+    end
+    % collect pixels of rect mask
+    rect_mask = ones(size(img_back));
+    for y = 1:y_ellipse_start-1
+        rect_mask(y, xleft_rect:W) = bw_mask(y,xleft_rect:W);
+    end
+    rect_mask = imcomplement(rect_mask);
+    % make ellipse mask complete
+    xcenter_ellipse = (xleft_ellipse + xright_ellipse) / 2;
     for y=y_ellipse_start:y_ellipse_stop
         for x=1:W
             v = bw_mask(y, x);
             if v == 0 && x < xcenter_ellipse
                 x1 = 2*xcenter_ellipse - x;
-                r = bw_mask(y, x1);
                 bw_mask(y, x1) = 0;
-                q = bw_mask(y, x1);
             end
         end
-        ellipse_mask(y,:) = bw_mask(y,:);
     end
+    % other figures (ellipse and scribble)
+    figures_mask = bw_mask + rect_mask;
+    figures_mask = imcomplement(figures_mask);
     M = S / num;
-    img_denoise = (ones(size(img_back)) * M) .* bw_mask;
-    bw_mask_inv = imcomplement(bw_mask);
-    img_figures = img_dbl .* bw_mask_inv;
-    img_result = img_denoise + img_figures;
+    img_denoise_bg = (ones(size(img_back)) * M) .* bw_mask;
+    img_figures = img_dbl .* figures_mask;
+    img_rect = img_dbl .* rect_mask;
+    img_result = img_denoise_bg + img_figures + img_rect;
     figure(5), imshow(img_result);
-    figure(3), imshow(ellipse_mask);
+    figure(2), imshow(img_rect);
+    figure(3), imshow(img_figures);
     imwrite(img_result, out_name, 'png');
 end
 
-function p = y_line_prod(y, mask)
+% does line y have at least one black point
+function p = y_line_black(y, mask)
     y_line_mask = mask(y, :);
     p = prod(y_line_mask);
+end
+
+function p = x_line_black(x, ymax, mask)
+    x_line_mask = mask(1:ymax, x);
+    p = prod(x_line_mask);
 end
